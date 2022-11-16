d20a9d883a20a72e0ed86c78d8a06a668b7e6474
jhso <sjh010@naver.com>
2022-11-16 11:43:31 +0900

    불필요 코드 삭제

--- src/test/java/com/apgroup/pms/PmsApplicationTests.java
+++ src/test/java/com/apgroup/pms/PmsApplicationTests.java
@@ -3,8 +3,6 @@ package com.apgroup.pms;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import com.apgroup.pms.thread.WorkTimer;
-
 @SpringBootTest
 class PmsApplicationTests {
 
